06be5b211560374079b0347bd39605c4ec166c2b
kiuwong <kiumwong@gmail.com>
2020-10-16 11:39:35 -0400

    Set up proxy

--- server/api/index.js
+++ server/api/index.js
@@ -25,7 +25,7 @@ app.get("*", (req, res) =>
 );
 
 if (process.env.NODE_ENV == "production") {
-  app.use('/', express.static('./build/'));
+  app.use('/', express.static('../../client/'));
   app.get("/*", function (request, response) {
     response.sendFile(path.join(__dirname, "build", "index.html"));
   });
