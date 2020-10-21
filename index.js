e380b5ce9eb57a65f01a2179e33a76ffade570fe
kiuwong <kiumwong@gmail.com>
2020-10-20 23:00:15 -0400

    deploy code

--- index.js
+++ index.js
@@ -25,12 +25,10 @@ app.get("*", (req, res) =>
   })
 );
 
-if (process.env.NODE_ENV == "production") {
-  app.use('/', express.static("./build/"));
+  app.use(express.static(path.join(__dirname, "build")));
   app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
   });
-}
 
 app.listen(port, () => {
   console.log(`Server is running on PORT ${port}`);
