cf1ec686ffa3df2d15382b628f2aeb413202bb8f
kiuwong <kiumwong@gmail.com>
2020-10-20 20:16:49 -0400

    fix

--- api/index.js
+++ api/index.js
@@ -30,9 +30,9 @@ app.get("*", (req, res) =>
 );
 
 if (process.env.NODE_ENV == "production") {
-  app.use('/', express.static("./build"));
+  app.use('/', express.static("./client/build"));
   app.get("/*", (req, res) => {
-    res.sendFile(path.join(newBase[0], "/client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "..", "/client/build", "index.html"));
   });
 }
 
